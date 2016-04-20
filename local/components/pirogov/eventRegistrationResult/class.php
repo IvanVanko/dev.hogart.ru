ea5f4a4f96dda19077efe59038d124f02f1c6e78
Ivan Koretskiy <gillbeits@gmail.com>
2016-04-20 12:32:33 +0300

    #46 fix redeclare class

--- local/components/pirogov/eventRegistrationResult/class.php
+++ local/components/pirogov/eventRegistrationResult/class.php
@@ -77,7 +77,7 @@ class EventRegistationResultComponent extends CBitrixComponent {
                 $barcode = $this->arResult['ELEMENT']['PROPERTIES']['BARCODE']['VALUE'];
             }
             $barcode = str_pad(substr($barcode, 0, 12), 12, "0", STR_PAD_LEFT);
-            include __DIR__."/ean.php";
+            include_once __DIR__."/ean.php";
             $ean = new EAN13($barcode, 2);
             $this->arResult['BARCODE'] = $ean->getBase64();
         }
