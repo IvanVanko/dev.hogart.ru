62f9a6ae60166196c07910c6037c16b6b35b55ed
Ivan Koretskiy <gillbeits@gmail.com>
2016-04-20 14:28:10 +0300

    test migration

---
+++ bitrix/php_interface/migrations/Version210420160001.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ivan Koretskiy aka gillbeits[at]gmail.com
+ * Date: 20/04/16
+ * Time: 14:24
+ */
+namespace Sprint\Migration;
+
+class Version210420160001 extends Version
+{
+    protected $description = "Обновления для задач 46, 26, 28";
+
+    public function up(){
+        //
+    }
+
+    public function down(){
+        //
+    }
+}
