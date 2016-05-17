e5941c5e47f7d74615f0a659cb92cc9a214d372b
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-17 23:06:22 +0300

    #66

--- local/templates/hogart/lang/en/header.php
+++ local/templates/hogart/lang/en/header.php
@@ -2,4 +2,6 @@
 $MESS ['Акции'] = "Special offers";
 $MESS ['Личный кабинет'] = "Account";
 $MESS ['Обратная связь'] = "Feedback";
-$MESS ['Отправить SMS'] = "Send SMS";
+$MESS ['Отправить SMS'] = "Share via SMS";
+$MESS ['Отправить на e-mail'] = "Share via Email";
+$MESS ['Распечатать'] = "Print";
