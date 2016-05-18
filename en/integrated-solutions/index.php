57f1838d6468889ee0d070bf6d20a3cecf510550
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-18 21:48:16 +0300

    #66 referenced elements

--- en/integrated-solutions/index.php
+++ en/integrated-solutions/index.php
@@ -26,9 +26,6 @@ $APPLICATION->SetTitle("Solutions");
     </p>
     </div>
     <br>
-    <!--  <div class="text-center inner no-padding no-full">--> <!--<div class="text-center inner no-full">
-    <img src="/images/complex_one.jpg" alt="" class="complex-page-img">
-  </div>-->
     <div class="inner no-full">
         <div class="icon-com">
             <h2>Ознакомьтесь с нашим передовым опытом поставок оборудования</h2>
@@ -36,7 +33,7 @@ $APPLICATION->SetTitle("Solutions");
                 "kontora:section.list",
                 "counter-com",
                 Array(
-                    "IBLOCK_ID" => PROJECT_TYPE_IBLOCK_ID,
+                    "IBLOCK_ID" => 35,
                     "PROPS" => "Y",
                     "SEF_MODE" => "Y",
                     "SEF_FOLDER" => "/en/integrated-solutions/",
@@ -50,7 +47,7 @@ $APPLICATION->SetTitle("Solutions");
                 "kontora:section.list",
                 "counter-zones",
                 Array(
-                    "IBLOCK_ID" => ZONE_IBLOCK_ID,
+                    "IBLOCK_ID" => 36,
                     "PROPS" => "Y",
                     "SEF_MODE" => "Y",
                     "SEF_FOLDER" => "/en/integrated-solutions/",
@@ -87,25 +84,7 @@ $APPLICATION->SetTitle("Solutions");
         <aside class="sidebar js-fh js-fixed-block js-paralax-height" data-fixed="top">
             <div class="inner js-paralax-item">
                 <div class="padding">
-                    <!--			 --><? //$APPLICATION->IncludeComponent(
-                    //	"bitrix:form.result.new",
-                    //	"integrated-solutions-form",
-                    //	Array(
-                    //		"WEB_FORM_ID" => "7",
-                    //		"IGNORE_CUSTOM_TEMPLATE" => "N",
-                    //		"USE_EXTENDED_ERRORS" => "N",
-                    //		"SEF_MODE" => "N",
-                    //		"VARIABLE_ALIASES" => Array("WEB_FORM_ID"=>"WEB_FORM_ID","RESULT_ID"=>"RESULT_ID"),
-                    //		"CACHE_TYPE" => "A",
-                    //		"CACHE_TIME" => "3600",
-                    //		"LIST_URL" => "",
-                    //		"EDIT_URL" => "",
-                    //		"SUCCESS_URL" => "",
-                    //		"CHAIN_ITEM_TEXT" => "",
-                    //		"CHAIN_ITEM_LINK" => ""
-                    //	),
-                    //$component
-                    //);
+                    <?
                     $form_id = "MAKE_REQUEST_" . strtoupper(LANGUAGE_ID);
                     if(BXHelper::can_show_form($form_id)) {
                         BXHelper::start_ajax_block();
@@ -129,43 +108,13 @@ $APPLICATION->SetTitle("Solutions");
                                     "WEB_FORM_ID" => "WEB_FORM_ID",
                                     "RESULT_ID" => "RESULT_ID"
                                 ),
-                                /*"DISPLAY_LEFT_COL" => array(
-                                    "FBK_NAME",
-                                    "FBK_PHONE",
-                                    "FBK_EMAIL",
-                                    "FBK_ORDER_WHERE",
-                                    "FBK_CITY",
-                                ),
-                                "DISPLAY_RIGHT_COL" => array(
-                                    "FBK_ORDER_DATE",
-                                    "FBK_ORDER_ID",
-                                    "FBK_COMMENT",
-                                    "FBK_FILE64",
-                                ),
-                                "MERGED_FIELDS" => array(
-                                    array("FBK_ORDER_DATE", "FBK_ORDER_ID")
-                                ),*/
-
                                 "CUSTOM_INPUT_PARAMS" => array(
                                     "SOLUTION_EMAIL" => array(
                                         "data-rule-email" => "true",
                                         "data-msg-email" => "Введите правильный email адрес"
                                     ),
                                 ),
-                                "CUSTOM_WRAPPER_PARAMS" => array(/* "SEMINAR_USER_EMAIL" => "data-clone-hidden=\"seminar_user_email\"",
-                            "SEMINAR_USER_PHONE" => "data-clone-hidden=\"seminar_user_phone\"",
-                            "SEMINAR_USER_CMP" => "data-clone-hidden=\"seminar_user_phone\"",
-                            "SEMINAR_USER_LNAME" => "data-clone=\"seminar_user_lname\"",
-                            "SEMINAR_USER_NAME" => "data-clone=\"seminar_user_name\"",
-                            "SEMINAR_USER_MNAME" => "data-clone=\"seminar_user_mname\"",
-                            "SEMINAR_USER_POST" => "data-clone=\"seminar_user_post\"",
-                            "SEMINAR_ID" => "data-clone=\"seminar_id\"",
-                            "SEMINAR_EAN_CODE" => "data-clone=\"seminar_ean_code\"",*/
-                                ),
-                                "CUSTOM_WRAPPER_CSS" => array(
-                                    "SOLUTION_PHONE" => "field custom_label phone"
-                                ),
-                                "CUSTOM_REQUIRED_MESS" => array(/*"SEMINAR_USER_LNAME" => "Пожалуйста, введите фамилию"*/
+                                "CUSTOM_WRAPPER_PARAMS" => array(
                                 ),
                                 "TITLE" => "Обратная связь",
                                 "SUCCESS_RELOAD" => "N",
