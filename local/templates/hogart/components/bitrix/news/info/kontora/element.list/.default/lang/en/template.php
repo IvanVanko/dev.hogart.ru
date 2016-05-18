c7ca748e18f2678b9c9f1aa070e3c8e28fcb1515
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-19 00:29:13 +0300

    #66 полезная информация

--- local/templates/hogart/components/bitrix/news/info/kontora/element.list/.default/lang/en/template.php
+++ local/templates/hogart/components/bitrix/news/info/kontora/element.list/.default/lang/en/template.php
@@ -1,3 +1,6 @@
 <?
 $MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "All the information linked to this record will be deleted. Continue anyway?";
+$MESS["Найти Статьи"] = "Search Articles";
+$MESS["Сбросить запрос"] = "Reset";
+$MESS["Бренд"] = "Brand";
 ?>
