15cff7294ce9d999b4d1d25a551f610d8c6acbc7
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-19 11:22:19 +0300

    #66

--- learn/detail.php
+++ learn/detail.php
@@ -1,49 +1,23 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Обучение");
-?>
-<?
 $APPLICATION->SetPageProperty("body_class","reg_page");
-/*$results=$_GET["RESULT_ID"];
-if ($results) {?>
-<div class="inner">
+?>
+<script>
 
-<?$APPLICATION->IncludeComponent("pirogov:custom.form.result.list", "", Array(
-        "SEF_MODE" => "Y",
-        "WEB_FORM_ID" => 5,
-    "FILTER" => array("ID"=>explode(",",$results)),
-    )
-);?>
-
-</div>*/?>
-<?/*} else {*/?>
-    <script>
-
-        $(document).ready(function () {
-            $('.empty-btn.black.to-otziv').click(function () {
-                var hrefData = $(this).attr('href'),
-                otzivTop = $(hrefData).offset().top;
-//                console.log($(this).attr('href'));
-                $('html, body').animate({
-                    scrollTop:otzivTop-$('.header-cnt').height()*2
-                }, 1000);
-                return false;
-            });
+    $(document).ready(function () {
+        $('.empty-btn.black.to-otziv').click(function () {
+            var hrefData = $(this).attr('href'),
+            otzivTop = $(hrefData).offset().top;
+            $('html, body').animate({
+                scrollTop:otzivTop-$('.header-cnt').height()*2
+            }, 1000);
+            return false;
         });
+    });
 
-    </script>
+</script>
 <div class="inner">
-<?
-//$APPLICATION->IncludeComponent(
-//	"kontora:element.detail",
-//	"seminar",
-//	Array(
-//		"ID" => $_REQUEST["CID"],
-//		"PROPS" => "Y",
-//		"PROPERTY_CODE" => Array("adress")
-//	)
-//);
-    ?>
 <?
     $APPLICATION->IncludeComponent(
         "bitrix:news.detail",
@@ -76,5 +50,4 @@ global $seminarTitle;
 if($seminarTitle)
     $APPLICATION->AddChainItem($seminarTitle);
 ?>
-<?/*}*/?>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
