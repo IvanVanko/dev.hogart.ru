4a983fbb29395c4b60cdf983c8b43aa30db97985
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-19 12:47:35 +0300

    #66

--- en/company/index.php
+++ en/company/index.php
@@ -1,8 +1,8 @@
 <? require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetPageProperty("keywords", "Компания Хогарт");
 $APPLICATION->SetPageProperty("description", "Компания Хогарт, основанная в 1996 году на сегодняшний день является одним из крупнейших поставщиков инженерных систем в области отопления, вентиляции и сантехники.");
-$APPLICATION->SetPageProperty("TITLE", "Компания");
-$APPLICATION->SetTitle("О компании Хогарт ");
+$APPLICATION->SetPageProperty("TITLE", "Company");
+$APPLICATION->SetTitle("About Hogart ");
 
 $APPLICATION->IncludeComponent("kontora:element.detail", "company", array(
 	"ID"    => "26247",
