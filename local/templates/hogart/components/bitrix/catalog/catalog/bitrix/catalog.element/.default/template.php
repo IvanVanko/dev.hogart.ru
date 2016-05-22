62c1acd4509f983fc47755ac31d00a1cae6a45ee
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-22 19:14:09 +0300

    #78 fix

--- local/templates/hogart/components/bitrix/catalog/catalog/bitrix/catalog.element/.default/template.php
+++ local/templates/hogart/components/bitrix/catalog/catalog/bitrix/catalog.element/.default/template.php
@@ -70,9 +70,35 @@ $this->setFrameMode(true);
                         </div>
                     </li>
                 <? } ?>
+                <? foreach ($arResult["DISPLAY_PROPERTIES"]["photos"]["VALUE"] as $photo): ?>
+                    <li>
+                        <div class="img-wrap">
+                            <img data-big-img="<?= CFile::GetPath($photo) ?>"
+                                 src="<?
+                                 $pic = CFile::ResizeImageGet(
+                                     $photo,
+                                     array("width" => 500, "height" => 228),
+                                     BX_RESIZE_IMAGE_PROPORTIONAL,
+                                     true);
+
+                                 echo $pic['src']; ?>" data-group="producPop"
+                                 class="js-popup-open-img" alt=""/>
+                        </div>
+                    </li>
+                <? endforeach; ?>
             </ul>
             <? if (count($arResult["DISPLAY_PROPERTIES"]["photos"]["VALUE"])): ?>
                 <div class="tumb-img">
+                    <?
+                    $photo_small = CFile::ResizeImageGet(
+                        $arResult['PREVIEW_PICTURE']['ID'] ? : $arResult['DETAIL_PICTURE']['ID'],
+                        array("width" => 110, "height" => 52),
+                        BX_RESIZE_IMAGE_EXACT,
+                        true
+                    );
+                    ?>
+                    <a data-slide-index="0" href=""><img src="<?= $photo_small['src'] ?>" alt=""/></a>
+                    
                     <? foreach ($arResult["DISPLAY_PROPERTIES"]["photos"]["VALUE"] as $key => $photo): ?>
                         <?
                         $photo_small = CFile::ResizeImageGet(
@@ -85,12 +111,12 @@ $this->setFrameMode(true);
                         <a data-slide-index="<?= $key ?>" href="<?=$arResult["DISPLAY_PROPERTIES"]["photos"]["FILE_VALUE"][$key]['SRC']?>"><img src="<?= $photo_small['src'] ?>" alt=""/></a>
                     <? endforeach; ?>
                 </div>
-                <?if (count($arResult["DISPLAY_PROPERTIES"]["photos"]["VALUE"]) > 1) {?>
+                <?if (count($arResult["DISPLAY_PROPERTIES"]["photos"]["VALUE"]) > 1): ?>
                     <div class="controls">
                         <div class="prev"></div>
                         <div class="next"></div>
                     </div>
-                <?}?>
+                <? endif; ?>
             <? endif; ?>
         </div>
     </div>
