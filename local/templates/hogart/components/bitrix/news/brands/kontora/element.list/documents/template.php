61d56151768fb737c94f6cde2bd2c6b0c0ac46ee
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-23 13:43:02 +0300

    #66

--- local/templates/hogart/components/bitrix/news/brands/kontora/element.list/documents/template.php
+++ local/templates/hogart/components/bitrix/news/brands/kontora/element.list/documents/template.php
@@ -1,27 +1,29 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 <aside class="sidebar js-fh js-fixed-block js-paralax-height" data-fixed="top">
     <div class="inner js-paralax-item">
-        <div class="side_href">
-            <a href="/documentation/?brands[]=<?=$arParams['FILTER']['PROPERTY_BRAND']?>&fbrand=y" class="icon_doc">Перейти к документации</a>
-        </div>
-        <?if (count($arResult["ITEMS"]) > 0):?>
-	        <div class="padding">
-	            <h2>Прайс - листы</h2>
-	            <ul class="price-list">
-	                <?foreach ($arResult["ITEMS"] as $arItem):
-	                	$arFile = CFile::GetFileArray($arItem['PROPERTIES']['file']['VALUE']);
-	                	$info = new SplFileInfo($arFile['FILE_NAME']);
-						$extention = $info->getExtension();?>
-		                <li>
-		                    <a target="_blank" href="<?=$arFile['SRC']?>">
-		                        <?=$arItem['NAME']?>
-		                    </a>
-		                    <span>— .<?=$extention?>, <?=round($arFile['FILE_SIZE']/1048576, 2)?> mb</span>
-		                </li>
-	                <?endforeach;?>
-	            </ul>
-	        </div>
-	    <?endif; ?>
+		<? if (LANGUAGE_ID != 'en'): ?>
+			<div class="side_href">
+				<a href="/documentation/?brands[]=<?=$arParams['FILTER']['PROPERTY_BRAND']?>&fbrand=y" class="icon_doc">Перейти к документации</a>
+			</div>
+			<?if (count($arResult["ITEMS"]) > 0):?>
+				<div class="padding">
+					<h2>Прайс - листы</h2>
+					<ul class="price-list">
+						<?foreach ($arResult["ITEMS"] as $arItem):
+							$arFile = CFile::GetFileArray($arItem['PROPERTIES']['file']['VALUE']);
+							$info = new SplFileInfo($arFile['FILE_NAME']);
+							$extention = $info->getExtension();?>
+							<li>
+								<a target="_blank" href="<?=$arFile['SRC']?>">
+									<?=$arItem['NAME']?>
+								</a>
+								<span>— .<?=$extention?>, <?=round($arFile['FILE_SIZE']/1048576, 2)?> mb</span>
+							</li>
+						<?endforeach;?>
+					</ul>
+				</div>
+			<?endif; ?>
+		<?endif; ?>
 		<div class="company-side-cnt padding text-center">
 			<h2><?= GetMessage("Основные направления деятельности") ?></h2>
 			<?$GetCurDir = explode("/", $APPLICATION->GetCurDir());
