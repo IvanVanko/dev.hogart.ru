d69a890ca8a97d68d87620dad5be036ade3cf74a
Ivan Koretskiy <gillbeits@gmail.com>
2016-05-25 23:47:52 +0300

    #92

--- local/templates/hogart/components/bitrix/catalog/catalog/section.php
+++ local/templates/hogart/components/bitrix/catalog/catalog/section.php
@@ -199,7 +199,7 @@ if ($isTableViewExt) {
     $order = "asc";
     $arParams["ELEMENT_SORT_FIELD2"] = "PROPERTY_collection.NAME";
     $arParams["ELEMENT_SORT_ORDER2"] = "ASC";
-    $arParams["PAGE_ELEMENT_COUNT"] = 1000;
+    $arParams["PAGE_ELEMENT_COUNT"] = 2000;
 }
 
 $intSectionID = $APPLICATION->IncludeComponent(
