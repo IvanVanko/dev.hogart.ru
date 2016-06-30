f0732bb83596812a0b4c97726736edb9f0ebdfd8
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-06-30 18:02:37 +0300

    HG-18

--- local/templates/hogart/js/main.js
+++ local/templates/hogart/js/main.js
@@ -38,7 +38,7 @@ HogartApp.prototype.setHandlers = function () {
         }
     });
 
-    $('a[href*="#"]:not([href="#"]):not([role="tab"])').click(function(event) {
+    $('a[href*="#"]:not([href="#"]):not([role="tab"]):not([data-toggle])').click(function(event) {
         if (window.location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && window.location.hostname == this.hostname) {
             event.preventDefault();
             var target = $(this.hash);
