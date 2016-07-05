f629b942205bd635569e063b8da003bdfcbcf32a
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-05 11:37:50 +0300

    HG-13 последняя итерация

--- local/php_interface/migrations/Version201607040001.php
+++ local/php_interface/migrations/Version201607040001.php
@@ -19,7 +19,7 @@ class Version201607040001 extends Version
     {
         $block_id = CATALOG_IBLOCK_ID;
         $arSelect = Array("ID", "IBLOCK_ID", "NAME", "IBLOCK_SECTION_ID", 'CODE');
-        $arFilter = Array("IBLOCK_ID"=>intval($block_id), "CODE"=>false /*'ID'=>30460*/);
+        $arFilter = Array("IBLOCK_ID"=>intval($block_id), "CODE"=>false);
 
         $res = (new \CIBlockElement)->GetList(Array(), $arFilter, false, false, $arSelect);
         var_dump($res->AffectedRowsCount());
@@ -28,28 +28,7 @@ class Version201607040001 extends Version
             $arFields = $ob->GetFields();
             if($arFields['CODE'] != '')
                 continue;
-            var_dump($arFields);
-
-//            $code = \CUtil::translit(trim($arFields['NAME']), 'ru',
-//                array('change_case' => 'L', 'replace_space' => '-', 'replace_other' => '-'));
-//            var_dump($code);
-//
-//            //проверяем наличие элемента с таким же кодом
-//            $rsItems = (new \CIBlockElement)->GetList(array(), array(
-//                'IBLOCK_ID' => $arFields['IBLOCK_ID'],
-//                "SECTION_ID" => $arFields['IBLOCK_SECTION_ID'],
-//                "!ID" => $arFields["ID"],
-//                "CODE" => $code
-//            ), false, false, array('ID'));
-//            if ($rsItems->AffectedRowsCount()) {
-//                $code = $code . uniqid("_");
-//            }
-//            var_dump($code);
             $update = (new \CIBlockElement)->Update($arFields['ID'], array('NAME'=>$arFields['NAME']), false, true);
-            $res2 = (new \CIBlockElement)->GetList(Array(), Array("IBLOCK_ID"=>intval($block_id), 'ID'=>30460), false, false, Array("ID", "NAME", 'CODE'));
-            $resitem = $res2->GetNextElement();
-            var_dump($resitem->GetFields());
-            exit();
         }
     }
 }
