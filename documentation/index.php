3c751aaee8ba7434b428e4a22b25f1f8418d50d5
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-10 01:40:15 +0300

    HG-19 fix title

--- documentation/index.php
+++ documentation/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetPageProperty("title", "Поиск документации");
+$APPLICATION->SetPageProperty("title", "Техническая документация");
 $APPLICATION->SetTitle("Документация");
 ?>
 
