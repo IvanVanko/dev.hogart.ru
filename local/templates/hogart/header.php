d1dc0eb27fabc461749080d968772e1ddf9e1472
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-20 10:14:26 +0300

    HG-39

--- local/templates/hogart/header.php
+++ local/templates/hogart/header.php
@@ -10,6 +10,14 @@ Loc::loadLanguageFile(__FILE__);
 <head>
     <meta charset="UTF-8">
     <meta name="mailru-verification" content="7d762af6578aa853"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
     <title><? $APPLICATION->ShowTitle("title") ?></title>
     <?
 
