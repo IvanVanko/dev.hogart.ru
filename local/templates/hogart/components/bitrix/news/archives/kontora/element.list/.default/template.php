95644328b0a54f9d4002b079009aa9c437e0e7bd
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-23 15:48:14 +0300

    HG-43 семинары #omfg

--- local/templates/hogart/components/bitrix/news/archives/kontora/element.list/.default/template.php
+++ local/templates/hogart/components/bitrix/news/archives/kontora/element.list/.default/template.php
@@ -3,45 +3,43 @@
 <div class="row">
     <div class="col-md-9">
         <h3><?$APPLICATION->ShowTitle()?></h3>
-        <? if (count($arResult["ITEMS"]) > 0): ?>
-            <ul class="list-base-learn">
-                <?foreach ($arResult["ITEMS"] as $arItem):
-                    $this->AddEditAction($arItem["ID"], $arItem["EDIT_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-                    $this->AddDeleteAction($arItem["ID"], $arItem["DELETE_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));?>
-                    <?$date = explode('.', $arItem['PROPERTIES']['sem_start_date']['VALUE']);?>
-                    <?$date_from = FormatDate("d F Y", MakeTimeStamp($arItem['PROPERTIES']['sem_start_date']['VALUE']));?>
-                    <?
-                    $date_sem_start = FormatDate("d.m.Y", MakeTimeStamp($arItem['PROPERTIES']['sem_start_date']['VALUE']));
-                    $date_sem_start = strtotime($date_sem_start);
-                    $date_sem_start =(!empty($date_sem_start))?$date_sem_start:0;
+        <ul class="list-base-learn">
+            <?foreach ($arResult["ITEMS"] as $arItem):
+                $this->AddEditAction($arItem["ID"], $arItem["EDIT_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem["ID"], $arItem["DELETE_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));?>
+                <?$date = explode('.', $arItem['PROPERTIES']['sem_start_date']['VALUE']);?>
+                <?$date_from = FormatDate("d F Y", MakeTimeStamp($arItem['PROPERTIES']['sem_start_date']['VALUE']));?>
+                <?
+                $date_sem_start = FormatDate("d.m.Y", MakeTimeStamp($arItem['PROPERTIES']['sem_start_date']['VALUE']));
+                $date_sem_start = strtotime($date_sem_start);
+                $date_sem_start =(!empty($date_sem_start))?$date_sem_start:0;
 
-                    $now=date($DB->DateFormatToPHP(CSite::GetDateFormat("SHORT")), time());
-                    $now=strtotime($now);
+                $now=date($DB->DateFormatToPHP(CSite::GetDateFormat("SHORT")), time());
+                $now=strtotime($now);
+                ?>
+                <li id="<?=$this->GetEditAreaId($arItem['ID'])?>">
+                    <?
+                    $file = CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'], array('width'=>100, 'height'=>100), BX_RESIZE_IMAGE_EXACT, true);
                     ?>
-                    <li id="<?=$this->GetEditAreaId($arItem['ID'])?>">
-                        <?
-                        $file = CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'], array('width'=>100, 'height'=>100), BX_RESIZE_IMAGE_EXACT, true);
-                        ?>
-                        <img src="<?=$file['src']?>" alt=""/>
-                        <a href="<?=$arItem['DETAIL_PAGE_URL']?>">
-                            <h4>
-                                <?=$arItem['NAME']?>
-                            </h4>
-                        </a>
-                        <div class="preview-txt">
-                            <?$obParser = new CTextParser;
-                            $arItem["PREVIEW_TEXT"] = $obParser->html_cut($arItem["PREVIEW_TEXT"], 400);?>
-                            <?=$arItem['PREVIEW_TEXT'];?>
-                        </div>
-                        <? if (!empty($arItem['PROPERTIES']['sem_start_date']['VALUE'])): ?>
-                            <time> — <?=$date_from;?></time>
-                        <? else: ?>
-                            <time>— Место проведения: </time><span>по запросу</span>
-                        <? endif; ?>
-                    </li>
-                <? endforeach; ?>
-            </ul>
-        <? endif; ?>
+                    <img src="<?=$file['src']?>" alt=""/>
+                    <a href="<?=$arItem['DETAIL_PAGE_URL']?>">
+                        <h4>
+                            <?=$arItem['NAME']?>
+                        </h4>
+                    </a>
+                    <div class="preview-txt">
+                        <?$obParser = new CTextParser;
+                        $arItem["PREVIEW_TEXT"] = $obParser->html_cut($arItem["PREVIEW_TEXT"], 400);?>
+                        <?=$arItem['PREVIEW_TEXT'];?>
+                    </div>
+                    <? if (!empty($arItem['PROPERTIES']['sem_start_date']['VALUE'])): ?>
+                        <time> — <?=$date_from;?></time>
+                    <? else: ?>
+                        <time>— Место проведения: </time><span>по запросу</span>
+                    <? endif; ?>
+                </li>
+            <? endforeach; ?>
+        </ul>
         <?= $arResult["NAV_STRING"]; ?>
     </div>
     <div class="col-md-3 aside">
