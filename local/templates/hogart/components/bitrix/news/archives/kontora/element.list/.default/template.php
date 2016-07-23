6e28c5d00d046b8aa667d9e7b6af1ee0834ff1e7
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-23 15:45:26 +0300

    HG-43 семинары

--- local/templates/hogart/components/bitrix/news/archives/kontora/element.list/.default/template.php
+++ local/templates/hogart/components/bitrix/news/archives/kontora/element.list/.default/template.php
@@ -3,14 +3,13 @@
 <div class="row">
     <div class="col-md-9">
         <h3><?$APPLICATION->ShowTitle()?></h3>
-        <?if (count($arResult["ITEMS"]) > 0):?>
+        <? if (count($arResult["ITEMS"]) > 0): ?>
             <ul class="list-base-learn">
                 <?foreach ($arResult["ITEMS"] as $arItem):
                     $this->AddEditAction($arItem["ID"], $arItem["EDIT_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
                     $this->AddDeleteAction($arItem["ID"], $arItem["DELETE_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));?>
                     <?$date = explode('.', $arItem['PROPERTIES']['sem_start_date']['VALUE']);?>
                     <?$date_from = FormatDate("d F Y", MakeTimeStamp($arItem['PROPERTIES']['sem_start_date']['VALUE']));?>
-                    <!--                <li data-date="--><?//=$arItem['PROPERTIES']['time']['VALUE'];?><!--">-->
                     <?
                     $date_sem_start = FormatDate("d.m.Y", MakeTimeStamp($arItem['PROPERTIES']['sem_start_date']['VALUE']));
                     $date_sem_start = strtotime($date_sem_start);
@@ -19,14 +18,11 @@
                     $now=date($DB->DateFormatToPHP(CSite::GetDateFormat("SHORT")), time());
                     $now=strtotime($now);
                     ?>
-                    <?//if ($date_sem_start < $now):?>
-
                     <li id="<?=$this->GetEditAreaId($arItem['ID'])?>">
                         <?
                         $file = CFile::ResizeImageGet($arItem['PREVIEW_PICTURE']['ID'], array('width'=>100, 'height'=>100), BX_RESIZE_IMAGE_EXACT, true);
                         ?>
                         <img src="<?=$file['src']?>" alt=""/>
-                        <!--                    <img src="--><?//=$arItem['PREVIEW_PICTURE']['SRC']?><!--" alt=""/>-->
                         <a href="<?=$arItem['DETAIL_PAGE_URL']?>">
                             <h4>
                                 <?=$arItem['NAME']?>
@@ -37,17 +33,16 @@
                             $arItem["PREVIEW_TEXT"] = $obParser->html_cut($arItem["PREVIEW_TEXT"], 400);?>
                             <?=$arItem['PREVIEW_TEXT'];?>
                         </div>
-                        <?if (!empty($arItem['PROPERTIES']['sem_start_date']['VALUE'])):?>
+                        <? if (!empty($arItem['PROPERTIES']['sem_start_date']['VALUE'])): ?>
                             <time> — <?=$date_from;?></time>
-                        <?else:?>
+                        <? else: ?>
                             <time>— Место проведения: </time><span>по запросу</span>
-                        <?endif;?>
+                        <? endif; ?>
                     </li>
-                    <?//endif;?>
-                <?endforeach;?>
+                <? endforeach; ?>
             </ul>
-        <?endif; ?>
-        <?=$arResult["NAV_STRING"];?>
+        <? endif; ?>
+        <?= $arResult["NAV_STRING"]; ?>
     </div>
     <div class="col-md-3 aside">
         <? if (!empty($arResult['FILTER']['BRANDS']) || (!empty($arResult["FILTER"]["DIRECTIONS"]) && LANGUAGE_ID != "en")): ?>
