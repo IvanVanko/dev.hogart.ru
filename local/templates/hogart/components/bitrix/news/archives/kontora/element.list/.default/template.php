99646a315dce85bf97e18432da9a1419fa08ceaa
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-23 15:55:22 +0300

    HG-43 семинары, странное поведение кэширования шаблона

--- local/templates/hogart/components/bitrix/news/archives/kontora/element.list/.default/template.php
+++ local/templates/hogart/components/bitrix/news/archives/kontora/element.list/.default/template.php
@@ -1,5 +1,18 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
-<?$page = $APPLICATION->GetCurDir(true);?>
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+$page = $APPLICATION->GetCurDir(true);
+?>
 <div class="row">
     <div class="col-md-9">
         <h3><?$APPLICATION->ShowTitle()?></h3>
--- local/templates/hogart/components/bitrix/news/archives/news.php
+++ local/templates/hogart/components/bitrix/news/archives/news.php
@@ -57,8 +57,7 @@ if ($APPLICATION->GetCurDir() === $arParams["SEF_FOLDER"]) {
             "NAV"           => "Y",
             "ELEMENT_COUNT" => 20,
             "FILTER"        => $arFilter,
-            'ORDER'         => $arParams['ORDER'],
-            "CACHE_TIME"    => 0
+            'ORDER'         => $arParams['ORDER']
         ),
         $component
     );
