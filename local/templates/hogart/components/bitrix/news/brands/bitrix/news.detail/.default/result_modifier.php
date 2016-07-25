c945ab00f0afcdbae57977918b38f8c9c23d1d5a
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-07-25 13:48:49 +0300

    HG-43 p8

--- local/templates/hogart/components/bitrix/news/brands/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/hogart/components/bitrix/news/brands/bitrix/news.detail/.default/result_modifier.php
@@ -69,38 +69,7 @@ while($next = $res->GetNext()) {
 BXHelper::addCachedKeys($this->__component, array('PRODUCT_GROUPS',
                                                   'PRODUCT_SECTION_GROUPS',
                                                   'PARENT_SECTIONS'), $arResult);
-/*$arSections = array();
-$rsSect = CIBlockSection::GetList(array("left_margin" => "asc"), array('ID' => $arSectionsId));
-while ($arSect = $rsSect->GetNext()) {
-	$arSections[] = $arSect;
+
+if (!preg_match("%^http[s]*%", $arResult['PROPERTIES']['site']['VALUE'])) {
+    $arResult['PROPERTIES']['site']['VALUE'] = "http://" . $arResult['PROPERTIES']['site']['VALUE'];
 }
-//Получить все родительские разделы этих секций
-//var_dump($arSections);
-foreach ($arSections as $arSection) {
-	$arFilter = array(
-		"IBLOCK_ID"      => 1,
-		'<=LEFT_BORDER'  => $arSection['LEFT_MARGIN'],
-		'>=RIGHT_BORDER' => $arSection['RIGHT_MARGIN'],
-		'>DEPTH_LEVEL'   => 1,
-
-	);
-	$rsSect = CIBlockSection::GetList(array("left_margin" => "asc"), $arFilter, true);
-	while ($arSect = $rsSect->GetNext()) {
-        $arSect['SECTION_PAGE_URL'] = HogartHelpers::rebuildBrandSectionHref($arParams["SEF_FOLDER"], $arResult["CODE"], $arSect["CODE"]);
-        $arFilter = Array("IBLOCK_ID"=>1, "PROPERTY_brand"=>$arResult["ID"]);
-        if ($arSect['ELEMENT_CNT']>0){
-
-            $arSect['COUNT_PRODS'] = CIBlockElement::GetList(Array(), $arFilter, array(), false, array());
-        }
-
-
-		if ($arSect['DEPTH_LEVEL'] == 2)
-
-                $arResult['PRODUCTS'][$arSect['ID']] = $arSect;
-
-		elseif ($arSect['DEPTH_LEVEL'] == 3)
-            if ($arSect['ELEMENT_CNT']>0) {
-                $arResult['PRODUCTS'][$arSect['IBLOCK_SECTION_ID']]['SECTIONS'][$arSect['ID']] = $arSect;
-            }
-	}
-}*/
