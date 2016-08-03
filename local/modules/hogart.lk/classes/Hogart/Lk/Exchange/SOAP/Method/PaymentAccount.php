10d52a551ffc427f93e3f6ed863f5f40316b91a7
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-08-03 17:58:17 +0300

    HG-24 PaymentAccount  включили отсылку Answer

--- local/modules/hogart.lk/classes/Hogart/Lk/Exchange/SOAP/Method/PaymentAccount.php
+++ local/modules/hogart.lk/classes/Hogart/Lk/Exchange/SOAP/Method/PaymentAccount.php
@@ -102,7 +102,7 @@ class PaymentAccount extends AbstractMethod
                 }
             }
         }
-//        $this->paymentAccountAnswer($answer);
+        $this->paymentAccountAnswer($answer);
         return count($answer->Response);
     }
 
