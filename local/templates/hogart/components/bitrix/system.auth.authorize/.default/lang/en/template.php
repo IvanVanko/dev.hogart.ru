9f892d6c94bc9d79aa8f15f0534ec63c16ade70b
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-08-27 20:06:45 +0300

    Форма авторизации

--- local/templates/hogart/components/bitrix/system.auth.authorize/.default/lang/en/template.php
+++ local/templates/hogart/components/bitrix/system.auth.authorize/.default/lang/en/template.php
@@ -2,7 +2,7 @@
 $MESS["AUTH_PLEASE_AUTH"] = "Please authorize:";
 $MESS["AUTH_LOGIN"] = "Login:";
 $MESS["AUTH_PASSWORD"] = "Password:";
-$MESS["AUTH_REMEMBER_ME"] = "Remember me on this computer";
+$MESS["AUTH_REMEMBER_ME"] = "Remember me";
 $MESS["AUTH_AUTHORIZE"] = "Authorize";
 $MESS["AUTH_REGISTER"] = "Register";
 $MESS["AUTH_FIRST_ONE"] = "If you are a first-time visitor on the site, please fill in the registration form.";
