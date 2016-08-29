da58be45294719f36245adf32762f6d807f469d1
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-08-29 07:38:42 +0300

    HG-25 заведение компонента Юр-Лиц

--- local/modules/hogart.lk/install/components/hogart.lk/account.documents/component.php
+++ local/modules/hogart.lk/install/components/hogart.lk/account.documents/component.php
@@ -1,6 +1,10 @@
 <?php
 use Hogart\Lk\Entity\AccountTable;
 use Hogart\Lk\Entity\AccountCompanyRelationTable;
+use Hogart\Lk\Entity\AddressTypeTable;
+use Hogart\Lk\Entity\AddressTable;
+use Hogart\Lk\Entity\CompanyTable;
+use Bitrix\Main\Type\Date;
 /**
  * Компонент отображения Юридических лиц
  * By: Ivan Kiselev aka shaqito[at]gmail.com
@@ -13,6 +17,60 @@ use Hogart\Lk\Entity\AccountCompanyRelationTable;
 if (!$this->initComponentTemplate())
     return;
 
+if (!empty($_SESSION["ACCOUNT_ID"]) && !empty($_POST)) {
+    switch ($_POST['action']){
+        case 'change_company':
+            $_SESSION['current_company_id'] = intval($_POST['cc_id']);
+            LocalRedirect($APPLICATION->GetCurPage());
+            die();
+            break;
+
+        case 'edit_company':
+            $_SESSION['current_company_id'] = intval($_POST['cc_id']);
+            break;
+        case 'add-company-fiz':
+            global $USER;
+            // @TODO: проверить доступ
+            $account = AccountTable::getAccountByUserID($USER->GetID());
+            if($account) {
+                $added_company_id = CompanyTable::add([
+                    'name' => $_POST['last_name'] . ' ' . $_POST['name'] . ' ' . $_POST['middle_name'],
+                    'type' => CompanyTable::TYPE_INDIVIDUAL,
+                    'doc_pass' => intval($_POST['doc_pass']),
+                    'doc_serial' => $_POST['doc_serial'],
+                    'doc_number' => $_POST['doc_number'],
+                    'doc_ufms' => $_POST['doc_ufms'],
+                    'doc_date' => new Date($_POST['doc_date'], 'd.m.Y'),
+                    'is_active' => true
+                ]);
+                AccountCompanyRelationTable::add([
+                    'company_id'=>$added_company_id,
+                    'account_id'=>$account['id']
+                ]);
+
+                foreach([AddressTypeTable::TYPE_ACTUAL, AddressTypeTable::TYPE_RESIDENTIAL] as $adrTypeCode){
+                    $address_type = AddressTypeTable::getByField('code',$adrTypeCode);
+                    $added_address_id = AddressTable::add([
+                        'owner_id' => $added_company_id,
+                        'owner_type' => AddressTable::OWNER_TYPE_CLIENT_COMPANY,
+                        'type_id' => $address_type['id'],
+                        'postal_code' => $_POST['postal_code'][$adrTypeCode],
+                        'region' => $_POST['region'][$adrTypeCode],
+                        'city' => $_POST['city'][$adrTypeCode],
+                        'street' => $_POST['street'][$adrTypeCode],
+                        'house' => $_POST['house'][$adrTypeCode],
+                        'building' => $_POST['building'][$adrTypeCode],
+                        'flat' => $_POST['flat'][$adrTypeCode],
+                        'is_active' => true
+                    ]);
+                }
+
+            }
+//            LocalRedirect($APPLICATION->GetCurPage());
+            die();
+            break;
+    }
+}
 if ($this->startResultCache()) {
     if (!CModule::IncludeModule("hogart.lk")) {
         $this->abortResultCache();
@@ -22,15 +80,28 @@ if ($this->startResultCache()) {
     global $CACHE_MANAGER;
 
     $account = AccountTable::getAccountByUserID($USER->GetID());
-
-    $account['companies'] = AccountCompanyRelationTable::getByAccountId($account['id']);
-    if(count($account['companies']) == 1)
-        $account['current_company'] = $account['companies'][0];
-    else
-        // @TODO: сделать выборку текущей компании из селекта
-        $account['current_company'] = AccountCompanyRelationTable::getCurrentCompany($account['company'][0]['COMPANY_id'], $account['id']);
-
     $arResult['account'] = $account;
+
+    // Юридические лица (они же компании)
+    $companies = AccountCompanyRelationTable::getByAccountId($account['id']);
+    if(count($companies) == 1)
+        $current_company = $companies[0];
+    else{
+        if(isset($_SESSION['cc_id']) && is_int($_SESSION['cc_id'])){
+            $current_company = AccountCompanyRelationTable::getCurrentCompany(intval($_SESSION['cc_id']), $account['id']);
+        }else{
+            $current_company = AccountCompanyRelationTable::getCurrentCompany($companies[0]['COMPANY_id'], $account['id']);
+        }
+    }
+
+    foreach($companies as $key=>$data){
+        if($data['COMPANY_id'] == $current_company['COMPANY_id'])
+            $companies[$key]['selected'] = 'selected';
+    }
+
+    $arResult['companies'] = $companies;
+    $arResult['current_company'] = $current_company;
+    var_dump($current_company);
 
     if (defined("BX_COMP_MANAGED_CACHE"))
     {
@@ -40,5 +111,6 @@ if ($this->startResultCache()) {
     }
     
     $this->includeComponentTemplate();
+
     return $account['id'];
 }
--- local/modules/hogart.lk/install/components/hogart.lk/account.documents/templates/.default/template.php
+++ local/modules/hogart.lk/install/components/hogart.lk/account.documents/templates/.default/template.php
@@ -4,6 +4,7 @@
  * Via: PhpStorm.
  * At: 18.08.2016 16:27
  */
+use Hogart\Lk\Entity\AddressTypeTable;
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -15,6 +16,124 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
+/** @ver array $companies */
 $this->setFrameMode(true);
 ini_set("xdebug.var_display_max_depth", -1);
-var_dump($arResult);
+//var_dump($arResult);
+?>
+
+<div class="row">
+    <div class="col-sm-12 col-xs-12">
+        <h3>Юридические лица и договора</h3>
+    </div>
+    <div class="col-sm-12 col-xs-12 authinfo">
+        <form method="post">
+            <input type="hidden" name="action" id="haction">
+        <div class="row spacer form-group">
+            <div class="col-sm-10 col-xs-10">
+                <select class="form-control" name="cc_id" id="current_company">
+                    <? foreach($arResult['companies'] as $company): ?>
+                        <option value="<?= $company['COMPANY_id']?>" <?= $company['selected']?>><?= $company['COMPANY_name']; ?></option>
+                    <? endforeach; ?>
+                </select>
+            </div>
+            <div class="col-sm-2 col-xs-2">
+                <?= \Hogart\Lk\Helper\Template\Dialog::Link('edit-company-dialog', 'edit', 'btn btn-default')?>
+                <?= \Hogart\Lk\Helper\Template\Dialog::Link('fav-company', 'fav', 'btn btn-'.($arResult['current_company']['is_favorite'] ? 'primary' : 'default' ))?>
+            </div>
+        </div>
+        </form>
+        <div class="row spacer">
+            <div class="col-sm-12 col-xs-12">
+                <?= \Hogart\Lk\Helper\Template\Dialog::Button('add-company-fiz-dialog', 'Добавить Физ. лицо', 'btn btn-primary')?>
+                <?= \Hogart\Lk\Helper\Template\Dialog::Button('add-company-ip-dialog', 'Добавить ИП', 'btn btn-primary')?>
+                <?= \Hogart\Lk\Helper\Template\Dialog::Button('add-company-ooo-dialog', 'Добавить Юр. лицо', 'btn btn-primary')?>
+            </div>
+        </div>
+    </div>
+</div>
+<? \Hogart\Lk\Helper\Template\Dialog::Start("add-company-fiz-dialog", [
+    'dialog-options' => 'closeOnConfirm: false',
+    'title' => 'Добавление физического лица'
+]) ?>
+<form action="<?= $APPLICATION->GetCurPage() ?>" name="add-company-fiz" method="post">
+    <input type="hidden" name="action" value="add-company-fiz">
+    <div class="form-group">
+        <input name="last_name" required="required" type="text" class="form-control" placeholder="Фамилия" data-error="Поле не должно быть пустым">
+        <input name="name" required="required" type="text" class="form-control" placeholder="Имя" data-error="Поле не должно быть пустым">
+        <input name="middle_name" type="text" class="form-control" placeholder="Отчество">
+    </div>
+    <div class="form-group">
+        <label class="control-label">Адрес прописки</label>
+        <input name="postal_code[<?= AddressTypeTable::TYPE_ACTUAL?>]" type="text" class="form-control" placeholder="Индекс">
+        <input name="city[<?= AddressTypeTable::TYPE_ACTUAL?>]" type="text" class="form-control" placeholder="Город">
+        <input name="street[<?= AddressTypeTable::TYPE_ACTUAL?>]" type="text" class="form-control" placeholder="Улица">
+        <input name="house[<?= AddressTypeTable::TYPE_ACTUAL?>]" type="text" class="form-control" placeholder="Дом">
+        <input name="flat[<?= AddressTypeTable::TYPE_ACTUAL?>]" type="text" class="form-control" placeholder="Квартира">
+    </div><div class="form-group">
+        <label class="control-label">Адрес проживания</label>
+        <input name="postal_code[<?= AddressTypeTable::TYPE_RESIDENTIAL?>]" type="text" class="form-control" placeholder="Индекс">
+        <input name="city[<?= AddressTypeTable::TYPE_RESIDENTIAL?>]" type="text" class="form-control" placeholder="Город">
+        <input name="street[<?= AddressTypeTable::TYPE_RESIDENTIAL?>]" type="text" class="form-control" placeholder="Улица">
+        <input name="house[<?= AddressTypeTable::TYPE_RESIDENTIAL?>]" type="text" class="form-control" placeholder="Дом">
+        <input name="flat[<?= AddressTypeTable::TYPE_RESIDENTIAL?>]" type="text" class="form-control" placeholder="Квартира">
+    </div>
+    <div class="form-group">
+        <label class="control-label">Телефон (моб.)</label>
+        <input name="phone[<?= \Hogart\Lk\Entity\ContactInfoTable::PHONE_KIND_MOBILE ?>]" data-mask="+7 (999) 999-99-99" type="text" class="form-control" placeholder="Телефон">
+    </div>
+    <div class="form-group">
+        <label class="control-label">Телефон (моб.)</label>
+        <input name="phone[<?= \Hogart\Lk\Entity\ContactInfoTable::PHONE_KIND_MOBILE ?>]" data-mask="+7 (999) 999-99-99" type="text" class="form-control" placeholder="Телефон">
+    </div>
+    <div class="form-group">
+        <label class="control-label">Email</label>
+        <input name="email" type="email" class="form-control" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <label class="control-label">Дата регистрации по месту жительства</label>
+        <input name="date_fact_address" type="text" class="form-control" placeholder="ДД/ММ/ГГГГ">
+    </div>
+    <div class="form-group">
+        <label class="control-label">Вид документа удостоверяющего личность</label>
+        <select name="doc_pass" class="form-control">
+            <option value="<?= \Hogart\Lk\Entity\CompanyTable::DOC_EMPTY?>">пустой</option>
+            <option value="<?= \Hogart\Lk\Entity\CompanyTable::DOC_PASSPORT?>">пасспорт</option>
+            <option value="<?= \Hogart\Lk\Entity\CompanyTable::DOC_NO_PASSPORT?>">другое</option>
+        </select>
+        <input name="doc_serial" type="text" class="form-control" placeholder="Серия">
+        <input name="doc_number" type="text" class="form-control" placeholder="Номер">
+        <input name="doc_ufms" type="text" class="form-control" placeholder="Кем выдан">
+        <input name="doc_date" type="text" class="form-control" placeholder="ДД/ММ/ГГГГ">
+    </div>
+    <input type="hidden" name="action" value="add-company-fiz">
+</form>
+<?
+$id = \Hogart\Lk\Helper\Template\Dialog::$id;
+$handler =<<<JS
+    (function() {
+      $('[data-remodal-id="$id"] form').validator();
+    })
+JS;
+\Hogart\Lk\Helper\Template\Dialog::Event('opening', $handler);
+$handler =<<<JS
+    (function() {
+      $('[data-remodal-id="$id"] form').submit();
+    })
+JS;
+\Hogart\Lk\Helper\Template\Dialog::Event('confirmation', $handler);
+\Hogart\Lk\Helper\Template\Dialog::End()
+?>
+
+<script lang="Javascript">
+    $(function() {
+        $('#current_company').change(function() {
+            $('#haction').val('change_company');
+            this.form.submit();
+        });
+        $('#edit_company').click(function() {
+            $('#haction').val('edit_company');
+            this.form.submit();
+        });
+    });
+</script>
