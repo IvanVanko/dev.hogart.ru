43adaca3abbe98e5a9dd4904402d51f2e7374ae8
Stanislav Sazonov <stas.sazonov@hogart.ru>
2016-11-08 13:21:31 +0300

    fix catalog amount for non auth user

--- local/templates/hogart/components/bitrix/catalog/catalog/bitrix/catalog.section/hogart_catalog_section/result_modifier.php
+++ local/templates/hogart/components/bitrix/catalog/catalog/bitrix/catalog.section/hogart_catalog_section/result_modifier.php
@@ -255,24 +255,24 @@ if ($account['id']) {
     }
 
     $storeAmounts = \Hogart\Lk\Entity\StoreAmountTable::getStoreAmountByItemsId(array_keys($arResult['ITEMS']), $storeFilter['ID']);
-}
 
-$prices = array_reduce($arResult['ITEMS'], function ($result, $item) {
-    $result[$item["ID"]] = $item["PRICES"]["BASE"]["VALUE"];
-    return $result;
-}, []);
+    $prices = array_reduce($arResult['ITEMS'], function ($result, $item) {
+        $result[$item["ID"]] = $item["PRICES"]["BASE"]["VALUE"];
+        return $result;
+    }, []);
 
-$prices = \Hogart\Lk\Entity\CompanyDiscountTable::prepareFrontByAccount($account['id'], $prices);
+    $prices = \Hogart\Lk\Entity\CompanyDiscountTable::prepareFrontByAccount($account['id'], $prices);
 
-foreach ($arResult['ITEMS'] as $id => &$item) {
-    $item["PRICES"]["BASE"]["VALUE"] = $prices[$id]['price'];
-    $item["PRICES"]["BASE"]["DISCOUNT_VALUE"] = $prices[$id]['price'];
-    $item["PRICES"]["BASE"]["DISCOUNT_DIFF"] = $prices[$id]['discount_amount'];
-    $item["PRICES"]["BASE"]["DISCOUNT_DIFF_PERCENT"] = (float)$prices[$id]['discount'];
-    $item["STORE_AMOUNTS"] = !empty($storeAmounts[$id]) ? $storeAmounts[$id] : [];
-    $item['CATALOG_QUANTITY'] = 0;
-    foreach ($item["STORE_AMOUNTS"] as $amount) {
-        $item['CATALOG_QUANTITY'] += $amount['stock'];
+    foreach ($arResult['ITEMS'] as $id => &$item) {
+        $item["PRICES"]["BASE"]["VALUE"] = $prices[$id]['price'];
+        $item["PRICES"]["BASE"]["DISCOUNT_VALUE"] = $prices[$id]['price'];
+        $item["PRICES"]["BASE"]["DISCOUNT_DIFF"] = $prices[$id]['discount_amount'];
+        $item["PRICES"]["BASE"]["DISCOUNT_DIFF_PERCENT"] = (float)$prices[$id]['discount'];
+        $item["STORE_AMOUNTS"] = !empty($storeAmounts[$id]) ? $storeAmounts[$id] : [];
+        $item['CATALOG_QUANTITY'] = 0;
+        foreach ($item["STORE_AMOUNTS"] as $amount) {
+            $item['CATALOG_QUANTITY'] += $amount['stock'];
+        }
     }
 }
 
@@ -288,6 +288,15 @@ while ($next = $prop_section_sort_result->fetch()) {
 
 $section_properties = BXHelper::getPropertySectionLinks($arParams['IBLOCK_ID'], $section_ids, 3, 3, array(), array('SMART_FILTER' => 'Y'));
 foreach ($arResult['ITEMS'] as $i => &$arCollItem) {
+    if (!isset($store_keys)) {
+        $store_keys = preg_grep("/^CATALOG_STORE_AMOUNT_/",array_keys($arCollItem));//подсчитываем кол-во товара только по отфильтрованным складам.
+    }
+    if (!empty($store_keys) && empty($account['id'])) {
+        $arCollItem['CATALOG_QUANTITY'] = 0;
+        foreach ($store_keys as $s_key) {
+            $arCollItem['CATALOG_QUANTITY'] +=intval($arCollItem[$s_key]);
+        }
+    }
     $result_properties = array();
     foreach ($section_properties as $key => $value) {
         if ($value['ID'] == $arCollItem['~IBLOCK_SECTION_ID'] && array_key_exists($value['CODE'], $arCollItem['PROPERTIES'])) {
