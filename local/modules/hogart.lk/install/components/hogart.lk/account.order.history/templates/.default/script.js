32df9e12200940a983bfde258385800d49313610
Stanislav Sazonov <stas.sazonov@hogart.ru>
2017-03-12 18:47:57 +0300

    HG-116 Адаптив ЛК

--- local/modules/hogart.lk/install/components/hogart.lk/account.order.history/templates/.default/script.js
+++ local/modules/hogart.lk/install/components/hogart.lk/account.order.history/templates/.default/script.js
@@ -11,18 +11,29 @@ var DataTableOptions = {
 };
 
 $(function () {
+  var promises = [];
+
   $('[data-table]').each(function (i, t) {
+    var dfd = $.Deferred();
+    promises.push(dfd);
     var table = $(t).DataTable(DataTableOptions);
+    $(table).on( 'draw.dt', function () {
+      dfd.resolve();
+    } );
   });
 
-  $('[data-relation-history]').each(function (i, el) {
-    var relationId = $(el).attr('data-relation-history');
-    if (!$(el).attr('data-relation-history')) return true;
+  $.when.apply($, promises).done(function () {
 
-    $('#svg').relationHistory('#' + relationId, '#' + $(el).attr('id'), {
-      rnd: 10,
-      strokeColor: $(el).attr('data-relation-history-color')
-    })
+    $('[data-relation-history]').each(function (i, el) {
+      var relationId = $(el).attr('data-relation-history');
+      if (!$(el).attr('data-relation-history')) return true;
+
+      $('#svg').relationHistory('#' + relationId, '#' + $(el).attr('id'), {
+        rnd: 10,
+        strokeColor: $(el).attr('data-relation-history-color')
+      })
+    });
+
   });
 
 });
