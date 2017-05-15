9de157865843e0d74a732db7d2e848e5fe58a3d2
Stanislav Sazonov <stas.sazonov@hogart.ru>
2017-05-15 14:53:31 +0300

    HG-134 Cтраница с прайс-листами для скачивания

---
+++ local/templates/hogart/components/kontora/element.list/price_brand_list/template.php
@@ -0,0 +1,25 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+
+?>
+
+<? foreach ($arResult["ITEMS"] as $arItem): ?>
+    <div class="col-md-6">
+        <a target="_blank" href="<?= $arItem["DETAIL_PAGE_URL"]?>">
+            <?= $arItem["NAME"] ?>
+        </a>
+    </div>
+<? endforeach; ?>
+
+
--- price/index.php
+++ price/index.php
@@ -33,21 +33,21 @@ $catalogMenu = $APPLICATION->IncludeComponent(
 
             <? foreach ($catalogMenu as $catalogItem): ?>
                 <li class="b-price-list__item">
-                    <a href="<?= $catalogItem[1] ?>" class="b-price-list__title" title="<?= $catalogItem[0] ?>">
+                    <a target="_blank" href="<?= $catalogItem[1] ?>" class="b-header__link" title="<?= $catalogItem[0] ?>">
                         <span><?= $catalogItem[0] ?></span>
                     </a>
 
                     <div class="b-price-list__information">
                         <? if (!empty($catalogItem[3]['PRICE_LIST_COVER'])): ?>
                             <? $file = CFile::ResizeImageGet($catalogItem[3]['PRICE_LIST_COVER'], array('width' => 300, 'height' => 424), BX_RESIZE_IMAGE_EXACT, true); ?>
-                            <a href="<?= CFile::GetPath($catalogItem[3]["PRICE"]); ?>" class="b-price-list__image">
+                            <a target="_blank" href="<?= CFile::GetPath($catalogItem[3]["PRICE"]); ?>" class="b-price-list__image">
                                 <img src="<?= $file['src']; ?>" alt="<?= $catalogItem[0] ?>">
                             </a>
                         <? endif; ?>
 
                         <? if(!empty($catalogItem[3]["PRICE"])): ?>
                             <? $priceFileMeta = CFile::MakeFileArray($catalogItem[3]["PRICE"]) ?>
-                            <a href="<?= CFile::GetPath($catalogItem[3]["PRICE"]); ?>" class="b-price-list__link" title="<?= $catalogItem[3]["PRICE_LABEL"] ?>">
+                            <a target="_blank" href="<?= CFile::GetPath($catalogItem[3]["PRICE"]); ?>" class="b-price-list__link" title="<?= $catalogItem[3]["PRICE_LABEL"] ?>">
                                 <span class="icon-acrobat">
                                     <?= $catalogItem[3]["PRICE_LABEL"] ?>
                                     <i class="download-link fa fa-download"></i>
@@ -62,6 +62,19 @@ $catalogMenu = $APPLICATION->IncludeComponent(
             <? endforeach; ?>
 
 		</ul>
+    </div>
+    <div class="col-md-3 col-xs-12 aside aside-mobile">
+        <div class="brand-links">
+            <div class="row">
+                <? $APPLICATION->IncludeComponent("kontora:element.list", "price_brand_list", array(
+                    'IBLOCK_ID' => BRAND_IBLOCK_ID,
+                    'ORDER' => array('NAME' => 'ASC'),
+                    'PROPS' => 'Y',
+                    "CACHE_TYPE" => "A",
+                    "CACHE_TIME" => "0",
+                ));?>
+            </div>
+        </div>
     </div>
 </div>
 
